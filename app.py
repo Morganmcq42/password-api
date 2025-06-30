2156ab0f6ab5d6d42a4c1bea8c3d04d49206afa4
Morganmcq42 <Morgan.McQueen@pepsico.com>
2025-06-30 16:20:27 -0500

    Add app.y and requirements.txt

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import random
+import string
+app = Flask(__name__)
+def generate_password():
+   length = 12
+   upper = random.choice(string.ascii_uppercase)
+   lower = random.choice(string.ascii_lowercase)
+   digit = random.choice(string.digits)
+   special = random.choice('!@#$%^&*')
+   all_chars = string.ascii_letters + string.digits + '!@#$%^&*'
+   remaining = [random.choice(all_chars) for _ in range(length - 4)]
+   password_list = list(upper + lower + digit + special + ''.join(remaining))
+   random.shuffle(password_list)
+   return ''.join(password_list)
+@app.route('/generate-password', methods=['GET'])
+def get_password():
+   return jsonify({'password': generate_password()})
+# ...existing code...
+@app.route('/', methods=['GET'])
+def home():
+    return 'Password API is running.'
+# ...existing code...
+if __name__ == '__main__':
+   app.run(port=5000)
