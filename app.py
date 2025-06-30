97976274066f36da0611c7689a0a31eb47b0bbcd
Morganmcq42 <Morgan.McQueen@pepsico.com>
2025-06-30 16:36:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -21,5 +21,6 @@ def get_password():
 def home():
     return 'Password API is running.'
 # ...existing code...
-if __name__ == '__main__':
-   app.run(port=5000)
+import os 
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
